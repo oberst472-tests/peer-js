b617d2cc9fbc0600c1a599c3a1718ebc2baee8bb
Александр Валесин <oberst47@gmail.com>
2020-11-20 17:53:05 +0300

    turn

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -31,9 +31,7 @@ export default {
             },
             constraints: {
                 iceServers: [
-                    {url: 'stun:stun1.l.google.com:19302'},
-                    {url: 'stun:stun2.l.google.com:19302'},
-                    {url: 'stun:stun3.l.google.com:19302'},
+                    { url: 'stun:vc-dev.enlighted.ru:3478' },
                     {
                         url: 'turn:vc-dev.enlighted.ru:3478',
                         username: 'tab1',
